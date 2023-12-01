8c128ef5e54fd6bfd72eee03451a6ae12c8c200e
Damian <damian.gorenski@blumentmt.com>
2023-12-01 18:15:35 +0100

    Added map class

---
+++ PrimeEngine-Game/Source/Map/Map.cpp
@@ -0,0 +1,25 @@
+﻿#include "Map.h"
+
+#include <iostream>
+
+#include "Tile.h"
+#include "Components/Sprite.h"
+#include "Managers/SpriteManager.h"
+
+namespace Application
+{
+    void Map::TickMap()
+    {
+    }
+
+    void Application::Map::DrawMap()
+    {
+    }
+
+    void Application::Map::RenderMap()
+    {
+        Tile* tile = new Tile(
+            "C:/Users/damia/OneDrive/Pulpit/PrimeEngine/PrimeEngine-Game/Assets/Map/punyworld-overworld-tileset_0.png",
+            Prime::Vector2().Zero());
+    }
+}
---
+++ PrimeEngine-Game/Source/Map/Map.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+namespace Application
+{
+    class Map
+    {
+    public:
+        Map() = default;
+
+        void TickMap();
+        void DrawMap();
+        void RenderMap();
+    private:
+        
+        
+    };
+}
+
+
