00a54f279fefad9e33e419ce387e4c63aee5a0ea
Damian <damian.gorenski@blumentmt.com>
2023-12-03 18:11:28 +0100

    Added random number generator

---
+++ PrimeEngine-Core/Source/Math/Prandom.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <random>
+
+namespace Prime::Math
+{
+    class Prandom
+    {
+    public:
+        static int GetRandomIntInRange(int min, int max)
+        {
+            std::random_device rd;
+            std::mt19937 eng(rd());
+            
+            std::uniform_int_distribution distr(min, max);
+
+            return distr(eng);
+        }
+        
+    };
+}
+
+
